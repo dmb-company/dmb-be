a636c1df8a6e209c17d6ebe85c3bf7a5f8d45e70
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-11-05 18:38:34 +0700

    fix: db connection

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,11 +2,10 @@ const Pool = require("pg").Pool;
 require("dotenv").config({ path: "./.env" });
 
 const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = {
